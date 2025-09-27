792bb2e7b64b9739506e0ac468322377149a655e
Sajad Danish <Sajaddanish256@gmail.com>
2025-09-27 17:17:30 +0430

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,7 @@ import styles from "./Style";
 export default function App() {
   return (
     <>
-      <div>
-        <h1 className={`${styles.paragraph} bg-black`}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni,
-          dolorem.
-        </h1>
-      </div>
+      <div></div>
     </>
   );
 }
